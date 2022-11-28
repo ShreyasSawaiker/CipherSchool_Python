719d0b07c4c29b000b4468ef943f09b42eff5693
Shreyas Sawaiker <mr.cyberwiz@gmail.com>
2022-11-28 23:14:58 +0530

    Activity

---
+++ Week1-Python-CipherSchools/class10-EscapeSequences-CipherSchools.py
@@ -0,0 +1,12 @@
+print("Hello \"World\" World") #Esacpe Sequence \
+print('I\'m Shreyas')
+
+print("Line A \nLine B") # New Line Escape Sequence
+
+print("I love \t Python") # \t TAB escape sqequence
+print("This is back slash \\") # single \
+print("This is double back slash \\\\")
+
+#Comments In Python #Single Line
+# ctrl + / short key or comment
+''' This is Multiline comment '''
