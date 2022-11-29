ca775654bec2b93f2c2d429e542e7df7d605f4c1
Shreyas Sawaiker <mr.cyberwiz@gmail.com>
2022-11-29 18:52:02 +0530

    Calculations

---
+++ class17-PythonCalculator-CipherSchools.py
@@ -0,0 +1,11 @@
+print(2+3*4)
+print(2/4) # floating point division
+print(2//4) # Integer division
+print(2**3) # Power 
+print(2**0.5)
+print(round(2**0.5,4))
+print(3%2) # Modulo Opertator
+
+print("Follows PEMDAS")
+print((2+3)/5)
+print(2**3**2)
