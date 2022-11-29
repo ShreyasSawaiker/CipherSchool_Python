81a3f2304a4cf07f6b1a94bebf15ae9595b63262
Shreyas Sawaiker <mr.cyberwiz@gmail.com>
2022-11-29 19:38:45 +0530

    Inputs

---
+++ Week1-Python-CipherSchools/class21,22-User_Input-CipherSchool.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter Num1:")) #int specifies integer else input takes string input
+num2 = int(input("Enter Num2:"))
+total = num1+num2
+
+print(total)
+
+num3 = str(30)
+num4 = float(50) #Type Conversions
+
+
+name,age = input("Enter Your Name and Age:").split()
+print(name)
+print(age)
