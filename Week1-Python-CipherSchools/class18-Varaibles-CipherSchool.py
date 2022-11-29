1303c46a4b9e6506c0ae297de61c8dfa9d4bb56b
Shreyas Sawaiker <mr.cyberwiz@gmail.com>
2022-11-29 19:32:09 +0530

    variables

---
+++ Week1-Python-CipherSchools/class18-Varaibles-CipherSchool.py
@@ -0,0 +1,20 @@
+number1 = 1
+print(number1)
+number1 = 3 # reassigning
+print(number1)
+
+name = "Shreyas"
+print(name)
+name = 555 # variables can be assigned any value, they are dynamic
+print(name)
+
+# 1user = "Name" variable cannot start with name
+# can use underscore at the start of the variabble
+
+_user2 = "Shrey"
+user1_rename = "Mr.Root"
+
+print(_user2,user1_rename)
+
+user_one_name = "Root" #Snake Case Styling
+UserOneName = "groot" #Camel Case
