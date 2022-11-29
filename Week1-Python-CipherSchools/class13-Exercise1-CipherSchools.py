f02da9957ee987b6b85eb1977e286944bfc24ef4
Shreyas Sawaiker <mr.cyberwiz@gmail.com>
2022-11-29 18:51:35 +0530

    excersise 1

---
+++ Week1-Python-CipherSchools/class13-Exercise1-CipherSchools.py
@@ -0,0 +1,11 @@
+'''**************************** Print Following ***********************'''
+# This is doube \\ backslah
+# These are /\/\/\/\/\ mountians
+# he is     awesome (use escape squence instead of normal space)
+# \" \n \t \' print this as output
+
+print("This is \\\\ double backslash")
+print("these are mountains/\\/\\/\\/\\")
+print("He is \t awesome")
+print("\\\" \\n \\t \\\'")
+print(r"This is \n backslash")
