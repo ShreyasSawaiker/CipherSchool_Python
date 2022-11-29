53528b7296d083b5ccd9a1327bc7630edf340096
Shreyas Sawaiker <mr.cyberwiz@gmail.com>
2022-11-29 19:59:31 +0530

    String Exercise

---
+++ Week1-Python-CipherSchools/class26-StringExercise-CipherShools.py
@@ -0,0 +1,5 @@
+num1=int(input())
+num2=int(input())
+num3=int(input())
+#  num1,num2,num3=int(input("comma seperated num").split(","))                     used for strings to seperate
+print(f"average is {(num1+num2+num3)/3}") 
