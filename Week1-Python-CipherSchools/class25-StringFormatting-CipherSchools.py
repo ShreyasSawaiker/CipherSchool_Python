88e8bf3719b777465f24187cccd91ae694a08e94
Shreyas Sawaiker <mr.cyberwiz@gmail.com>
2022-11-29 19:49:16 +0530

    Formats

---
+++ Week1-Python-CipherSchools/class25-StringFormatting-CipherSchools.py
@@ -0,0 +1,6 @@
+name="Anurag"
+age=18
+print("hello " + name + " your age is" + str(age)) # normal hard sytax
+print("hello {} your age is {}".format(name,age)) #python3 syntax
+
+print(f"hello {name} and you age is {age}") # python3.6 modern syntax
