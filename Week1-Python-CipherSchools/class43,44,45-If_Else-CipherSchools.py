58540726323cf1e1699db748aac13e2c10dcef4e
Shreyas Sawaiker <mr.cyberwiz@gmail.com>
2022-11-30 18:58:10 +0530

    If Else Stats

---
+++ Week1-Python-CipherSchools/class43,44,45-If_Else-CipherSchools.py
@@ -0,0 +1,14 @@
+#if else statements example
+
+age = input("enter your age: ")
+age = int(age)
+if age>= 14:
+    print("your are about 14")
+else:
+    print("you are below 14")
+#else statement can be used only after if statement
+
+#pass statements
+x = 18
+if x>= 14:
+    pass # used to skip the loop
