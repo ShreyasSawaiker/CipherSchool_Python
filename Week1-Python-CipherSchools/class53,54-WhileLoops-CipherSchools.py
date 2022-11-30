f63a9d3e01d217e1080a5594d8416cbc43de3e55
Shreyas Sawaiker <mr.cyberwiz@gmail.com>
2022-11-30 19:58:10 +0530

    While Loops

---
+++ Week1-Python-CipherSchools/class53,54-WhileLoops-CipherSchools.py
@@ -0,0 +1,14 @@
+#<-----------------------------------while loop---------------------------------->
+i=1
+while i<=10:
+    print(f"HI{i}")
+    i+=1
+
+
+# Programe To Count total
+total=0
+i=1
+while i<=10:
+    total=total+i
+    i=i+1
+print(total)
