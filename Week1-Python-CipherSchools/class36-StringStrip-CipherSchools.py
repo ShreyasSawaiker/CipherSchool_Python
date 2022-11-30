45efd702f6500a0ba3f54a41f74c3f7a83638d69
Shreyas Sawaiker <mr.cyberwiz@gmail.com>
2022-11-30 18:29:19 +0530

    Strip Function

---
+++ Week1-Python-CipherSchools/class36-StringStrip-CipherSchools.py
@@ -0,0 +1,7 @@
+name = "             Harshit            "
+dots = "..................."
+
+print(name + dots)
+print(name.lstrip() + dots)#removes left spaces
+print(name.rstrip() + dots)#removes right spaces
+print(name.strip() + dots)
